0def15490b01fd5f441b2b09319634fad14b37a4
Sean Daniel Celerio <sdcelerio@gmail.com>
2026-02-15 01:13:59 -0600

    Made CMake project for c++ code. Might delete python code. Code so far outputs the recording and calculates PCA, but still need to output the vectors to verify the calculations are correct. Also need to attempt live camera recordings.

---
+++ src/PCA.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <deque>
+#include <filesystem>
+#include <dv-processing/core/core.hpp>
+#include <dv-processing/io/camera/discovery.hpp>            // Used for real-time readings
+#include <dv-processing/io/mono_camera_recording.hpp>       // Used for reading .aedat4 recordings
+#include <dv-processing/visualization/event_visualizer.hpp> // Used to generate images to display
+#include <dv-processing/core/stream_slicer.hpp>             // Used to collect readings 
+#include <opencv4/opencv2/highgui.hpp>                      // Used to display the data
+
+#define ROLLING_WINDOW_SIZE 5000
+#define SLICER_SIZE ROLLING_WINDOW_SIZE/4
+
+void Calculate_PCA(double Cov_XX, double Cov_XY, double Cov_YY, double Eigenvalues[], double Eignevector_1[], double Eigenvector_2[]) {
+    // Use quadratic formula to obtain eigenvalues
+    double a = 1;
+    double b = -(Cov_XX + Cov_YY);
+    double c = ((Cov_XX * Cov_YY) - (Cov_XY * Cov_XY));
+    double Eigenvalue_1 = (-b + sqrt((b * b) - (4 * a * c)))/(2 * a);
+    double Eigenvalue_2 = (-b - sqrt((b * b) - (4 * a * c)))/(2 * a);
+
+    // Create vectors and return 
+    double Magnitude = sqrt(pow(Eigenvalue_1 - Cov_YY, 2) + pow(Cov_XY, 2));
+    Eignevector_1[0] = (Eigenvalue_1 - Cov_YY)/Magnitude; 
+    Eignevector_1[1] = Cov_XY/Magnitude;
+
+    Magnitude = sqrt(pow(Eigenvalue_2 - Cov_YY, 2) + pow(Cov_XY, 2));
+    Eigenvector_2[0] = (Eigenvalue_2 - Cov_YY)/Magnitude; 
+    Eigenvector_2[1] = Cov_XY/Magnitude;
+}
+
+int main(void) {
+    // Initialize the reader for the file
+    std::filesystem::path filePath = "/mnt/c/Users/sdcel/OneDrive/Documents/School_Work/College_Work/4th_Year/2026_Spring/ENGR_491_512/Camera_Assignments/Ruler.aedat4";
+    dv::io::MonoCameraRecording Reader(filePath);
+    if (!Reader.isEventStreamAvailable()) {
+        std::cerr << "Error! Could not find any events in the filepath " << filePath << std::endl;
+        return 1;
+    }   
+
+    // Intialize the Visualizer
+    dv::visualization::EventVisualizer visualizer(Reader.getEventResolution().value(), dv::visualization::colors::black,
+        dv::visualization::colors::green, dv::visualization::colors::red);
+    
+    // Intialize the Event Slicer
+    dv::EventStreamSlicer Slicer;
+    std::deque<dv::Event> Rolling_Window;
+    double Mean_X = 0; double Mean_Y = 0;
+    double Sum_X = 0; double Sum_Y = 0;
+    double Cov_XX = 0; double Cov_XY = 0; double Cov_YY = 0;
+    Slicer.doEveryNumberOfElements(SLICER_SIZE, [&](const dv::EventStore &Events) {
+        for (const dv::Event& New_Event : Events) {
+            // Push the new event into the rolling window
+            Rolling_Window.push_back(New_Event);
+            double New_X = New_Event.x();
+            double New_Y = New_Event.y();
+            std::size_t Current_Size = Rolling_Window.size();
+
+            // If the rolling window went over its defined size, remove the oldest event and update means/covariances 
+            if (Current_Size > ROLLING_WINDOW_SIZE) {
+                double Old_X = Rolling_Window.front().x();
+                double Old_Y = Rolling_Window.front().y();
+                Rolling_Window.pop_front();
+                double Old_Mean_X = Mean_X;
+                double Old_Mean_Y = Mean_Y;
+
+                Sum_X += New_X;
+                Sum_Y += New_Y;
+
+                Mean_X += (New_X/ROLLING_WINDOW_SIZE) - (Old_X/ROLLING_WINDOW_SIZE);
+                Mean_Y += (New_Y/ROLLING_WINDOW_SIZE) - (Old_Y/ROLLING_WINDOW_SIZE);
+
+                Cov_XX += ((New_X - Mean_X) * (New_X - Mean_X) / (ROLLING_WINDOW_SIZE - 1)) - ((Old_X - Old_Mean_X) * (Old_X - Old_Mean_X) / (ROLLING_WINDOW_SIZE - 1));
+                Cov_XY += ((New_X - Mean_X) * (New_Y - Mean_Y) / (ROLLING_WINDOW_SIZE - 1)) - ((Old_X - Old_Mean_X) * (Old_Y - Old_Mean_Y) / (ROLLING_WINDOW_SIZE - 1));
+                Cov_YY += ((New_Y - Mean_Y) * (New_Y - Mean_Y) / (ROLLING_WINDOW_SIZE - 1)) - ((Old_Y - Old_Mean_Y) * (Old_Y - Old_Mean_Y) / (ROLLING_WINDOW_SIZE - 1));
+            }
+            // If the rolling window is growing, update mean/covariances
+            else if (Current_Size > 1) {
+                Sum_X += New_X;
+                Sum_Y += New_Y;
+
+                Mean_X += (New_Event.x() - Mean_X) / Current_Size;
+                Mean_Y += (New_Event.y() - Mean_Y) / Current_Size;
+
+                Cov_XX = (Sum_X - Mean_X * Current_Size) * (Sum_X - Mean_X * Current_Size) / (ROLLING_WINDOW_SIZE - 1);
+                Cov_XY = (Sum_X - Mean_X * Current_Size) * (Sum_Y - Mean_Y * Current_Size) / (ROLLING_WINDOW_SIZE - 1);
+                Cov_YY = (Sum_Y - Mean_Y * Current_Size) * (Sum_Y - Mean_Y * Current_Size) / (ROLLING_WINDOW_SIZE - 1);
+            }
+            // Else its the first entry to the rolling window so initialize means/variances
+            else {
+                Sum_X = New_X;
+                Sum_Y = New_Y;
+
+                Mean_X = New_X;
+                Mean_Y = New_Y;
+            }
+        }
+
+        // Calculate the PCA values 
+        double Eigenvalues[2];
+        double Vector_1[2];
+        double Vector_2[2];
+        Calculate_PCA(Cov_XX, Cov_XY, Cov_YY, Eigenvalues, Vector_1, Vector_2);
+        
+        std::cout << Vector_1[0] << " " << Vector_1[1] << std::endl;
+        
+        // Produce frame and output
+        cv::Mat frame = visualizer.generateImage(Events);
+        cv::Point center(static_cast<int>(Mean_X), static_cast<int>(Mean_Y)); // Draw a solid blue circle at the mean
+        cv::circle(frame, center, 5, cv::Scalar(255, 0, 0), -1);
+        cv::imshow("Real-Time 500 Events", frame);
+        cv::pollKey();
+    });
+    
+    // Loop to feed event readings into the slicer 
+    while(Reader.isRunning()) {
+       if (auto Events = Reader.getNextEventBatch()) 
+            Slicer.accept(*Events);
+    }
+
+    return 0;
+}
