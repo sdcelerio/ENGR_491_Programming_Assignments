7d1dee85a054e4526b2363bee84fb89eebc37a1a
Sean Daniel Celerio <sdcelerio@gmail.com>
2026-02-17 15:53:55 -0600

    Added guard to std::sqrt() from returning NAN due to negative numbers. Also changes to how slicer takes information from number to time. Needs a check to see if the function is run with no events causing std::out_of_range error for vectors. And add magnitude protection for PCA function.

--- src/PCA.cpp
+++ src/PCA.cpp
@@ -9,12 +9,16 @@
 #include <opencv4/opencv2/highgui.hpp>                      // Used to display the data
 
 #define ROLLING_WINDOW_SIZE 20000
-#define SLICER_SIZE 2500
+#define SLICER_SIZE 5000
 
 void Calculate_PCA(const double Cov_XX, const double Cov_XY, const double Cov_YY, double Eigenvalues[], double Eignevector_1[], double Eigenvector_2[]);
 void Draw_Vector(cv::Mat& Frame, double Center_X, double Center_Y, double Vector[], double Magnitude, cv::Scalar Color);
 
 int main(void) {
+    // Open the first camera that appears
+    dv::io::camera::CameraPtr Camera = dv::io::camera::open();
+    
+    /*
     // Initialize the reader for the file
     std::filesystem::path filePath = "/mnt/c/Users/sdcel/OneDrive/Documents/School_Work/College_Work/4th_Year/2026_Spring/ENGR_491_512/Camera_Assignments/Ruler.aedat4";
     dv::io::MonoCameraRecording Reader(filePath);
@@ -22,9 +26,10 @@ int main(void) {
         std::cerr << "Error! Could not find any events in the filepath " << filePath << std::endl;
         return 1;
     }   
-
+    */
+    
     // Intialize the Visualizer
-    dv::visualization::EventVisualizer visualizer(Reader.getEventResolution().value(), dv::visualization::colors::black,
+    dv::visualization::EventVisualizer visualizer(Camera->getEventResolution().value(), dv::visualization::colors::black,
         dv::visualization::colors::green, dv::visualization::colors::red);
     
     // Intialize the Event Slicer
@@ -33,47 +38,44 @@ int main(void) {
     double Mean_X = 0; double Mean_Y = 0;
     double Sum_X = 0; double Sum_Y = 0; double Sum_XX = 0; double Sum_XY = 0; double Sum_YY = 0;
     double Cov_XX = 0; double Cov_XY = 0; double Cov_YY = 0;
-    Slicer.doEveryNumberOfElements(SLICER_SIZE, [&](const dv::EventStore &Events) {
+
+    std::chrono::microseconds time(100000);
+    Slicer.doEveryTimeInterval(time, [&](const dv::EventStore &Events) {
         for (const dv::Event& New_Event : Events) {
             // Push the new event into the rolling window
             Rolling_Window.push_back(New_Event);
             double New_X = New_Event.x();
             double New_Y = New_Event.y();
-            std::size_t Current_Size = Rolling_Window.size();
             Sum_X  += New_X;
             Sum_Y  += New_Y;
             Sum_XX += New_X * New_X;
             Sum_YY += New_Y * New_Y;
             Sum_XY += New_X * New_Y;
 
-            // If the rolling window went over its defined size, remove the oldest event and update means/covariances 
-            if (Current_Size > ROLLING_WINDOW_SIZE) {
+            // If the rolling window went over its defined size, remove the oldest event from the sum values
+            if (Rolling_Window.size() > ROLLING_WINDOW_SIZE) {
                 double Old_X = Rolling_Window.front().x();
                 double Old_Y = Rolling_Window.front().y();
                 Rolling_Window.pop_front();
-                double Old_Mean_X = Mean_X;
-                double Old_Mean_Y = Mean_Y;
 
-                Mean_X += (New_X/ROLLING_WINDOW_SIZE) - (Old_X/ROLLING_WINDOW_SIZE);
-                Mean_Y += (New_Y/ROLLING_WINDOW_SIZE) - (Old_Y/ROLLING_WINDOW_SIZE);
-
-                Cov_XX += ((New_X - Mean_X) * (New_X - Mean_X) / (ROLLING_WINDOW_SIZE - 1)) - ((Old_X - Old_Mean_X) * (Old_X - Old_Mean_X) / (ROLLING_WINDOW_SIZE - 1));
-                Cov_XY += ((New_X - Mean_X) * (New_Y - Mean_Y) / (ROLLING_WINDOW_SIZE - 1)) - ((Old_X - Old_Mean_X) * (Old_Y - Old_Mean_Y) / (ROLLING_WINDOW_SIZE - 1));
-                Cov_YY += ((New_Y - Mean_Y) * (New_Y - Mean_Y) / (ROLLING_WINDOW_SIZE - 1)) - ((Old_Y - Old_Mean_Y) * (Old_Y - Old_Mean_Y) / (ROLLING_WINDOW_SIZE - 1));
+                Sum_X  -= Old_X;
+                Sum_Y  -= Old_Y;
+                Sum_XX -= Old_X * Old_X;
+                Sum_XY -= Old_X * Old_Y;
+                Sum_YY -= Old_Y * Old_Y;
             }
-            // If the rolling window is growing, update mean/covariances
-            else if (Current_Size > 1) {
-                Mean_X += (New_X - Mean_X) / Current_Size;
-                Mean_Y += (New_Y - Mean_Y) / Current_Size;
+            
+            // Calculate Mean and Covariance on the fly for the current state of the rolling window
+            std::size_t Window_Size = Rolling_Window.size();
+            
+            Mean_X = Sum_X / Window_Size;
+            Mean_Y = Sum_Y / Window_Size;
 
-                Cov_XX = (Sum_XX - (Sum_X * Sum_X) / Current_Size) / (Current_Size - 1);
-                Cov_YY = (Sum_YY - (Sum_Y * Sum_Y) / Current_Size) / (Current_Size - 1);
-                Cov_XY = (Sum_XY - (Sum_X * Sum_Y) / Current_Size) / (Current_Size - 1);
-            }
-            // Else its the first entry to the rolling window so initialize means/variances
-            else {
-                Mean_X = New_X;
-                Mean_Y = New_Y;
+            // Variance/Covariance requires at least 2 elements (N-1 degrees of freedom)
+            if (Window_Size > 1) {
+                Cov_XX = (Sum_XX - (Sum_X * Sum_X) / Window_Size) / (Window_Size - 1);
+                Cov_YY = (Sum_YY - (Sum_Y * Sum_Y) / Window_Size) / (Window_Size - 1);
+                Cov_XY = (Sum_XY - (Sum_X * Sum_Y) / Window_Size) / (Window_Size - 1);
             }
         }
 
@@ -86,16 +88,16 @@ int main(void) {
         // Produce frame and output
         cv::Mat frame = visualizer.generateImage(Events);
         cv::Point center(static_cast<int>(Mean_X), static_cast<int>(Mean_Y)); // Draw a solid blue circle at the mean
-        Draw_Vector(frame, Mean_X, Mean_Y, Vector_1, sqrt(Eigenvalues[0]), cv::Scalar(255, 255, 255));
-        Draw_Vector(frame, Mean_X, Mean_Y, Vector_2, sqrt(Eigenvalues[1]), cv::Scalar(255, 255, 255));
+        Draw_Vector(frame, Mean_X, Mean_Y, Vector_1, std::sqrt(Eigenvalues[0]), cv::Scalar(255, 255, 255));
+        Draw_Vector(frame, Mean_X, Mean_Y, Vector_2, std::sqrt(Eigenvalues[1]), cv::Scalar(255, 255, 255));
         cv::circle(frame, center, 5, cv::Scalar(255, 0, 0), -1);
         cv::imshow("Real-Time 500 Events", frame);
         cv::pollKey();
     });
     
     // Loop to feed event readings into the slicer 
-    while(Reader.isRunning()) {
-       if (auto Events = Reader.getNextEventBatch()) 
+    while(Camera->isRunning()) {
+       if (std::optional<dv::EventStore> Events = Camera->getNextEventBatch()) 
             Slicer.accept(*Events);
     }
 
@@ -106,17 +108,24 @@ void Calculate_PCA(double Cov_XX, double Cov_XY, double Cov_YY, double Eigenvalu
     // Use quadratic formula to obtain eigenvalues (a = 1)
     double b = -(Cov_XX + Cov_YY);
     double c = ((Cov_XX * Cov_YY) - (Cov_XY * Cov_XY));
-    Eigenvalues[0] = (-b + sqrt((b * b) - (4 * c)))/(2.0);
-    Eigenvalues[1] = (-b - sqrt((b * b) - (4 * c)))/(2.0);
+    double Discriminant = std::max(0.0, (b * b) - (4. * c)); // Used to prevent imaginary numbers and NAN return by std::sqrt
+    Eigenvalues[0] = (-b + std::sqrt(Discriminant))/(2.0);
+    Eigenvalues[1] = (-b - std::sqrt(Discriminant))/(2.0);
     
     // Create vectors and return 
-    double Magnitude = sqrt(pow(Eigenvalues[0] - Cov_YY, 2) + pow(Cov_XY, 2));
+    double Magnitude = std::sqrt(std::pow(Eigenvalues[0] - Cov_YY, 2) + std::pow(Cov_XY, 2));
     Eignevector_1[0] = (Eigenvalues[0] - Cov_YY)/Magnitude; 
     Eignevector_1[1] = Cov_XY/Magnitude;
+    if (Magnitude == 0) {
+        std::cout << "This is the problem" << std::endl;
+    }
     
-    Magnitude = sqrt(pow(Eigenvalues[1] - Cov_YY, 2) + pow(Cov_XY, 2));
+    Magnitude = std::sqrt(std::pow(Eigenvalues[1] - Cov_YY, 2) + std::pow(Cov_XY, 2));
     Eigenvector_2[0] = (Eigenvalues[1] - Cov_YY)/Magnitude; 
     Eigenvector_2[1] = Cov_XY/Magnitude;
+    if (Magnitude == 0) {
+        std::cout << "This is the problem" << std::endl;
+    }
 }
 
 void Draw_Vector(cv::Mat& Frame, double Center_X, double Center_Y, double Vector[], double Magnitude, cv::Scalar Color) {
